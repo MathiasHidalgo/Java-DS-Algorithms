2e4b7597c00718e028b6c6b314f2b7e4e8472fef
MathiasHidalgo <mejhc2307lbc@gmail.com>
2023-10-28 21:13:45 -0400

    Calculator

---
+++ Calc/src/Calculator/Calculator.java
@@ -0,0 +1,186 @@
+package Calculator;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Calculator extends Container {
+
+    private double total1 = 0.0;
+    private double total2 = 0.0;
+    private char math_operator;
+    private JPanel Calculator;
+    private JTextField textDisplay;
+    private JButton btnDivide;
+    private JButton btnOne;
+    private JButton btnTwo;
+    private JButton btnFour;
+    private JButton btnSeven;
+    private JButton btnZero;
+    private JButton btnPlus;
+    private JButton btnFive;
+    private JButton btnEight;
+    private JButton btnPoint;
+    private JButton btnThree;
+    private JButton btnSix;
+    private JButton btnNine;
+    private JButton btnClear;
+    private JButton btnMinus;
+    private JButton btnMultiply;
+    private JButton btnEquals;
+
+    private void getOperator (String btnText) {
+        math_operator = btnText.charAt(0);
+        total1 = total1 + Double.parseDouble(textDisplay.getText());
+        textDisplay.setText("");
+    }
+
+    public Calculator() {
+
+        btnOne.addActionListener(new ActionListener() { // Every time you press the button, in this case btnOne that has the text 1
+            @Override
+            public void actionPerformed(ActionEvent e) { // execute a method
+                /* this variable will store a string data type, textDisplay is nothing because the output field is nothing ""
+                Then btnOne.getText is accessing at the btnOne text that is 1
+                * */
+                String btnOneText = textDisplay.getText() + btnOne.getText();
+                textDisplay.setText(btnOneText); // overall this display in the output field the number 1
+            }
+        });
+
+        // The same with the all the rest of the button, because it's just creating a method to access the data inside the button
+        btnTwo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnTwoText = textDisplay.getText() + btnTwo.getText();
+                textDisplay.setText(btnTwoText);
+            }
+        });
+
+        btnThree.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnThreeText = textDisplay.getText() + btnThree.getText();
+                textDisplay.setText(btnThreeText);
+            }
+        });
+
+        btnFour.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnFourText = textDisplay.getText() + btnFour.getText();
+                textDisplay.setText(btnFourText);
+            }
+        });
+
+        btnFive.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnFiveText = textDisplay.getText() + btnFive.getText();
+                textDisplay.setText(btnFiveText);
+            }
+        });
+
+        btnSix.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnSixText = textDisplay.getText() + btnSix.getText();
+                textDisplay.setText(btnSixText);
+            }
+        });
+
+        btnSeven.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnSevenText = textDisplay.getText() + btnSeven.getText();
+                textDisplay.setText(btnSevenText);
+            }
+        });
+
+        btnEight.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnEightText = textDisplay.getText() + btnEight.getText();
+                textDisplay.setText(btnEightText);
+            }
+        });
+
+        btnNine.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnNineText = textDisplay.getText() + btnNine.getText();
+                textDisplay.setText(btnNineText);
+            }
+        });
+
+        btnZero.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String btnZeroText = textDisplay.getText() + btnZero.getText();
+                textDisplay.setText(btnZeroText);
+            }
+        });
+
+        btnPoint.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if(textDisplay.getText().equals("")){
+                    textDisplay.setText("0.");
+                }else if (textDisplay.getText().contains(".")){
+                    btnPoint.setEnabled(false);
+                }else{
+                    String btnPointText = textDisplay.getText() + btnPoint.getText();
+                    textDisplay.setText(btnPointText);
+                }
+                btnPoint.setEnabled(true);
+            }
+        });
+
+        btnPlus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String button_text = btnPlus.getText(); // store the operator
+                getOperator(button_text); // get the method declared at the beginning
+            }
+        });
+
+        btnMinus.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                /*This total 1 store the number you just put*/
+                total1 = total1 - Double.parseDouble(textDisplay.getText());
+                textDisplay.setText(""); // and then the textDisplay field "output field" get empty
+            }
+        });
+
+        btnEquals.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                /* This one take the total 1 (first button you press before add and that is store in total 1) and then
+                * take the number inside the button you took before pressing equal and after plus*/
+                total2 = total1 + Double.parseDouble(textDisplay.getText());
+                textDisplay.setText(Double.toString(total2)); // display the result of the sum
+                total1 = 0; // and the set the total 1 to 0
+            }
+        });
+
+        // This obviously set it up to 0 and no empty
+        btnClear.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                total2 = 0.0;
+                textDisplay.setText("");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Calculator");
+        frame.setContentPane(new Calculator().Calculator);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
